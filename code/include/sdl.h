1feea81c9d185a3884c02b6179c6edc41b93a2d2
Ptitmorceaux <106253875+ptitmorceaux@users.noreply.github.com>
2025-10-05 07:43:22 +0200

    render sdl menu (first step)

--- code/include/sdl.h
+++ code/include/sdl.h
@@ -3,7 +3,12 @@
 
     #include "global.h"
 
-    int initSDL();
-    void quitSDL();
+    int initSDL(SDL_Window** window, SDL_Renderer** renderer);
+    void quitSDL(SDL_Window* window, SDL_Renderer* renderer);
+
+    TTF_Font* loadFont(const char* fontPath, int fontSize);
+
+    void renderBlackScreen(SDL_Renderer* renderer);
+    void renderMenu(SDL_Renderer* renderer, TTF_Font* font, char** options, size_t nbOptions, size_t selected);
 
 #endif
