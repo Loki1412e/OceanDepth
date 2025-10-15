f22f84ddd3a6079d5e4017f6360e9735d36b6b86
Loki <manille001@gmail.com>
2025-10-15 22:05:08 +0200

    effets.h/.c: add enumSpecialEffectToChar and charToEnumSpecialEffect

--- code/src/display.c
+++ code/src/display.c
@@ -14,23 +14,6 @@ void printListSave(ListeSauvegardes *saves);
 
 void printSave(Sauvegarde *save);
 
-char *enumSpecialEffectToChar(EffetsSpeciaux special_effect);
-
-
-char *enumSpecialEffectToChar(EffetsSpeciaux type) {
-    switch (type) {
-        case AUCUN: return "AUCUN";
-        case BENEDICTION_OCEAN: return "BENEDICTION_OCEAN";
-        case MALEDICTION_OCEAN: return "MALEDICTION_OCEAN";
-        case SAIGNEMENT: return "SAIGNEMENT";
-        case PARALYSIE: return "PARALYSIE";
-        case ETREINTE: return "ETREINTE";
-        case PRECISION_REDUITE: return "PRECISION_REDUITE";
-        case DEFENSE_AUGMENTEE: return "DEFENSE_AUGMENTEE";
-        case VOIX_DU_COURANT: return "VOIX_DU_COURANT";
-    }
-}
-
 
 /*====== Temp ======*/
 
