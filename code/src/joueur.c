8f44ef2a198a3631a37b93b778c7dca75d52f348
Ptitmorceaux <trucknotsus@gmail.com>
2025-10-20 22:54:08 +0200

    update fopen player -> plongeur/stats.conf

--- code/src/joueur.c
+++ code/src/joueur.c
@@ -42,7 +42,7 @@ Plongeur *initDiver(char *diver_name) {
 
 // Pas encore les compétences a voir plus tard...
 int setDiverFromConf(Plongeur *diver) {
-    FILE *f = fopen("config/plongeur.conf", "r");
+    FILE *f = fopen("config/plongeur/stats.conf", "r");
     if (f == NULL) return EXIT_FAILURE;
 
     char line[256];
