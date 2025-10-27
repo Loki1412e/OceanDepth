f08c7d7a34789209a7b9eaaeb00e607ab9a60321
Ptitmorceaux <trucknotsus@gmail.com>
2025-10-28 00:15:25 +0100

    add setListeConsommableFromConf && initModalListeConsommable

--- code/include/consommables.h
+++ code/include/consommables.h
@@ -6,7 +6,16 @@
 
     void freeConsommable(Consommable *c);
     void freeListeConsommables(ListeConsommable *liste);
+    void freeListeConsommablesContent(ListeConsommable *liste);
     
+    ListeConsommable *initModalListeConsommable(char *path);
+    int setListeConsommableFromConf(ListeConsommable *modalConsumables, char *path);
+
+    int ajouterConsommable(ListeConsommable *modal, ListeConsommable *list, size_t id_consommable);
     int utiliserConsommable(ListeConsommable *list, Consommable *c, void *user_ptr, EntiteType user_type);
+    int supprimerConsommable(ListeConsommable *list, Consommable *c);
+
+    int consommableInList(ListeConsommable *list, Consommable *c);
+    Consommable *duplicateConsommable(Consommable *c);
 
 #endif
