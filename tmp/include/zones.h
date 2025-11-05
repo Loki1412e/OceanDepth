a4a8fb4b3454b89f4544c4fb979cafed4daf5432
Ptitmorceaux <trucknotsus@gmail.com>
2025-11-05 03:26:46 +0100

    Add support for tracking and saving cleared cells in TierMap

--- tmp/include/zones.h
+++ tmp/include/zones.h
@@ -5,12 +5,13 @@
 
     int startGame();
     
-    int loadPlayerProgress(PlayerProgress* p);
-    void savePlayerProgress(const PlayerProgress* p);
+    int loadPlayerProgress(PlayerProgress* p, TierMap* m);
+    int savePlayerProgress(PlayerProgress* p, TierMap* m);
 
     Zone generate_zone(int index);
     void show_zone(const Zone* z);
     void free_tier(TierMap *m);
+    int mark_cell_as_cleared(TierMap *m, int r, int c);
     void build_tier(int tier, unsigned int seed, TierMap *m, int start_col);
     void draw_tier(const TierMap *m, int player_row, int player_col);
     void spawn_monsters(TierMap *m, int tier);
