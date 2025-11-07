9a54935cc2cbeaaf68b3579733dfbc69d945e586
Ptitmorceaux <106253875+ptitmorceaux@users.noreply.github.com>
2025-11-07 20:01:59 +0100

    update initTier function to include isNewTier parameter and improve memory management in free functions

--- code/include/zones.h
+++ code/include/zones.h
@@ -10,7 +10,7 @@
     void free_tier(TierMap *m);
     void free_player_progress(PlayerProgress *p);
 
-    TierMap *initTier(PlayerProgress *player_progress);
+    TierMap *initTier(PlayerProgress *player_progress, short isNewTier);
 
     TierMap *build_tier(int tier, unsigned seed, PlayerProgress *p, short isNewTier);
     Zone generate_zone(int index);
